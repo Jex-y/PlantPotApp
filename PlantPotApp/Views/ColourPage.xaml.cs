fe3741deca9f1150abf710dcb43af65c10b583fa
Edward Jex <edwardjex@live.co.uk>
2020-05-15 11:40:54 +0100

    Add project files.

---
+++ PlantPotApp/Views/ColourPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PlantPotApp.Views
+namespace xmlns:utmdev="clr-namespace:Utmdev.Xf.Components;assembly=Utmdev.Xf.ColorWheel"
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+    }
+}
